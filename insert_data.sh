4e3c74ef90e038ec909e4da750b4bc71ec4078d1
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-11-30 01:12:39 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -9,3 +9,24 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+
+echo $($PSQL "TRUNCATE TABLES games, teams;")
+
+
+while IFS=',' read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; 
+do
+    
+ if [[ $WINNER != "winner" && $WINNER != "opponent"  ]]; then
+ TEAM_NAME=$($PSQL "SELECT name FROM teams WHERE name=$WINNER")
+  if [[ -z $TEAM_NAME ]]; then
+  INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+  fi
+ fi
+if [[ $OPPONENT != "winner" && $OPPONENT != "opponent" ]]; then
+ TEAM_NAME=$($PSQL "SELECT name FROM teams WHERE name=$OPPONENT")
+ if [[ -z $TEAM_NAME ]]; then
+ INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+ fi
+fi 
+done < games.csv
+
